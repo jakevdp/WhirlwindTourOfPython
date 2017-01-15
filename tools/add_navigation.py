d442439057f2754a2c2b9808f0f8642b82986460
Jake VanderPlas <jakevdp@gmail.com>
2017-01-15 06:26:42 -0800

    Add navigation to notebooks

---
+++ tools/add_navigation.py
@@ -0,0 +1,71 @@
+import os
+import itertools
+import re
+
+from ipykernel import kernelspec as ks
+import nbformat
+from nbformat.v4.nbbase import new_markdown_cell
+
+
+NOTEBOOK_DIR = os.path.join(os.path.dirname(__file__), '..')
+REG = re.compile(r'(\d\d)-(.*)\.ipynb')
+
+
+def iter_notebooks():
+    return sorted(nb for nb in os.listdir(NOTEBOOK_DIR) if REG.match(nb))
+
+
+def get_notebook_title(nb_file):
+    nb = nbformat.read(os.path.join(NOTEBOOK_DIR, nb_file), as_version=4)
+    for cell in nb.cells:
+        if cell.source.startswith('#'):
+            return cell.source[1:].splitlines()[0].strip()
+
+
+def prev_this_next(it):
+    a, b, c = itertools.tee(it,3)
+    next(c)
+    return zip(itertools.chain([None], a), b, itertools.chain(c, [None]))
+
+
+PREV_TEMPLATE = "< [{title}]({url}) "
+CONTENTS = "| [Contents](Index.ipynb) |"
+NEXT_TEMPLATE = " [{title}]({url}) >"
+NAV_COMMENT = "<!--NAVIGATION-->\n"
+
+
+def iter_navbars():
+    for prev_nb, nb, next_nb in prev_this_next(iter_notebooks()):
+        navbar = NAV_COMMENT
+        if prev_nb:
+            navbar += PREV_TEMPLATE.format(title=get_notebook_title(prev_nb),
+                                           url=prev_nb)
+        navbar += CONTENTS
+        if next_nb:
+            navbar += NEXT_TEMPLATE.format(title=get_notebook_title(next_nb),
+                                           url=next_nb)
+        yield os.path.join(NOTEBOOK_DIR, nb), navbar
+
+
+def write_navbars():
+    for nb_name, navbar in iter_navbars():
+        nb = nbformat.read(nb_name, as_version=4)
+        nb_file = os.path.basename(nb_name)
+        is_comment = lambda cell: cell.source.startswith(NAV_COMMENT)
+
+        if is_comment(nb.cells[1]):
+            print("- amending navbar for {0}".format(nb_file))
+            nb.cells[1].source = navbar
+        else:
+            print("- inserting navbar for {0}".format(nb_file))
+            nb.cells.insert(1, new_markdown_cell(source=navbar))
+
+        if is_comment(nb.cells[-1]):
+            nb.cells[-1].source = navbar
+        else:
+            nb.cells.append(new_markdown_cell(source=navbar))
+        nbformat.write(nb, nb_name)
+
+
+if __name__ == '__main__':
+    write_navbars()
